d5765d2381e51c1437d976dc6abe31024c068894
miles-grant-ibi <miles.grant@ibigroup.com>
2021-07-12 14:38:09 -0400

    fix(commit): use cjs to determine commitizen path

--- src/mastarm/commit.ts
+++ src/mastarm/commit.ts
@@ -1,11 +1,10 @@
-import path from 'path'
-
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap
 
 export default (): void => {
+  // Needed to make it work directly when calling "tsdx commit"
   process.argv.pop()
   bootstrap({
-    cliPath: path.join(__dirname, '../../node_modules/commitizen'),
+    cliPath: require('commitizen'),
     config: {
       path: 'cz-conventional-changelog'
     }
