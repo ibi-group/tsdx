c3c0efce147162e36be954f9ddb63d3e7d93e10d
miles-grant-ibi <miles.grant@ibigroup.com>
2021-07-12 14:42:25 -0400

    revert(commit): due to implementation issue, commit only works once installed

--- src/mastarm/commit.ts
+++ src/mastarm/commit.ts
@@ -1,10 +1,12 @@
+import path from 'path'
+
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap
 
 export default (): void => {
   // Needed to make it work directly when calling "tsdx commit"
   process.argv.pop()
   bootstrap({
-    cliPath: require('commitizen'),
+    cliPath: path.join(__dirname, '../../../../node_modules/commitizen'),
     config: {
       path: 'cz-conventional-changelog'
     }
